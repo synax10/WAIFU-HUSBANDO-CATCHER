99bf03cbddf0a13c3ee308219a77283d2ec933e1
synax10 <150779327+synax10@users.noreply.github.com>
2023-12-23 15:32:54 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -281,7 +281,7 @@ async def start(update: Update, context: CallbackContext) -> None:
             [InlineKeyboardButton("SUPPORT", url=f'https://t.me/{SUPPORT_CHAT}'),
             InlineKeyboardButton("UPDATES", url=f'https://t.me/{UPDATE_CHAT}')],
             [InlineKeyboardButton("ADD ME", url=f'http://t.me/{BOT_USERNAME}?startgroup=new')],
-            [InlineKeyboardButton("SOURCE", url=f'https://github.com/MyNameIsShekhar/WAIFU-HUSBANDO-CATCHER')],
+            [InlineKeyboardButton("SOURCE", url=f'https://t.me/synax_chats')],
             
         ]
         
@@ -325,7 +325,7 @@ async def button(update: Update, context: CallbackContext) -> None:
             [InlineKeyboardButton("SUPPORT", url=f'https://t.me/{SUPPORT_CHAT}'),
             InlineKeyboardButton("UPDATES", url=f'https://t.me/{UPDATE_CHAT}')],
             [InlineKeyboardButton("HELP", callback_data='help')],
-            [InlineKeyboardButton("SOURCE", url=f'https://github.com/MyNameIsShekhar/WAIFU-HUSBANDO-CATCHER')]
+            [InlineKeyboardButton("SOURCE", url=f'https://t.me/synax_chats')]
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
 
